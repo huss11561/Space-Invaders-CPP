c9cb66f41e2fcb314d0611ffbaca697d0b16cab4
huss11561 <hussein56456@gmail.com>
2024-08-29 01:21:53 +0200

    trackpad logik als kommentar

--- src/controller_console.cpp
+++ src/controller_console.cpp
@@ -10,3 +10,12 @@ wchar_t ConsoleController::getInput() {
     return ch;
     return ch;
 };
+
+/*
+ * **Alternative Controller-Implementierung: Trackpad-Controller**
+ *
+ * Anstelle der Tastatursteuerung kann ein Trackpad-Controller verwendet werden, um Eingaben über ein Trackpad oder Touchscreen zu verarbeiten. 
+ * Der Trackpad-Controller würde Eingaben wie Wischbewegungen und Tippen erfassen und diese in allgemeine Steuerbefehle übersetzen. 
+ * Diese Befehle werden dann an das Modell weitergegeben, um die Spielfigur zu steuern. 
+ * Dadurch wird das Modell von der spezifischen Implementierung des Controllers entkoppelt und kann flexibler auf verschiedene Eingabemethoden reagieren.
+ */
