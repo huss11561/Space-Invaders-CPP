7b59a9316259e44d1d2366011dabfa3f4251fcc1
gzi890 <n_akco01@uni-muenster.de>
2024-08-31 12:21:10 +0200

    Update stone.cpp
    dox added

--- src/stone.cpp
+++ src/stone.cpp
@@ -1,26 +1,32 @@
 // Stone.cpp
 #include "stone.h"
 
+// Constructor for the Stone class, initializing the stone's position (x, y) and health
 Stone::Stone(int x, int y, int health) : x(x), y(y), health(health) {}
 
+// Getter function to retrieve the x-coordinate of the stone
 int Stone::getX() const {
     return x;
 }
 
+// Getter function to retrieve the y-coordinate of the stone
 int Stone::getY() const {
     return y;
 }
 
+// Getter function to retrieve the current health of the stone
 int Stone::getHealth() const {
     return health;
 }
 
+// Reduces the health of the stone by 1 when the stone takes damage
 void Stone::takeDamage() {
-    if (health > 0) {
+    if (health > 0) { // Ensure health does not go below 0
         health--;
        }
 }
 
+// Returns true if the stone's health is zero or less, indicating it is broken
 bool Stone::isBroken() const {
     return health <= 0;
 }
